1bc2614648ac626d7297aa6f7f4e747083116c3c
RyanSchrodinger <ronaldsagan000@gmail.com>
2026-08-17 15:04:07 -0300

    feat: add navegacao na tela _Layout

---
+++ Uc_10_Ryan_00003_Razor/Migrations/20260817175946_tb_livro .cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Uc_10_Ryan_00003_Razor.Migrations
+{
+    /// <inheritdoc />
+    public partial class tb_livro : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Coffe",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nome = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Descricao = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Preco = table.Column<decimal>(type: "decimal(18,2)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Coffe", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Coffe");
+        }
+    }
+}
