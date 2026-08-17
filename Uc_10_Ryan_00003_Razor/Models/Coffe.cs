20ce94d7ac17d9d6ce1a895818340ab556a1c60c
RyanSchrodinger <ronaldsagan000@gmail.com>
2026-08-17 14:56:37 -0300

    feat: add model referente coffe

---
+++ Uc_10_Ryan_00003_Razor/Models/Coffe.cs
@@ -0,0 +1,11 @@
+﻿namespace Uc_10_Ryan_00003_Razor.Models
+{
+    public class Coffe
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Descricao { get; set; }
+        public decimal Preco { get; set; }
+       
+    }
+}
